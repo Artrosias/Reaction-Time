7c5ffcec706742c317dfead248dd1cbc0438beac
Alessio Guidotti <ale99_sami03@hotmail.com>
2023-03-23 18:55:35 +0100

    Aggiornamento

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome</p>
+  <div v-if="showModal" >
+    <Modal theme="" @close="toggleModal">
+      <template v-slot:links >
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Ninja Givaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </div>
+
+  <div v-if="showModalTwo" >
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up to the newsletter</h1>
+      <p>For updates and promo cosdes!</p>
+    </Modal>
+  </div>
+<button  @click.alt="toggleModal">open modal (alt)</button>
+<button  @click="toggleModalTwo">open modal</button>
+</template>
+
+<script> // JAVASCRIPT PATH
+import Modal from './components/Modal.vue'
+
+
+export default { // ROOT COMPONENT
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App',
+      showModal: false,
+      showModalTwo: false,
+    }
+  },
+  methods: {
+    toggleModal () {
+      this.showModal = !this.showModal
+
+    },
+    toggleModalTwo(){
+      this.showModalTwo = !this.showModalTwo
+    }
+  }
+}
+</script>
+
+<style> /* CSS PATH */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
